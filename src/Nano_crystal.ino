a965b801ba7b4aafc4af66261ad291eda2aa8ac9
Leor Yamay <leor.yamay@gmail.com>
2022-11-25 20:02:22 +0200

    ok

--- src/Nano_crystal.ino
+++ src/Nano_crystal.ino
@@ -1,8 +1,10 @@
 #include <FastLED.h>
+#include <SoftwareSerial.h>
+bool off = true;
 
 #define ONBOARD_LED 13
 #define POWER_ON 7
-#define POWER_SWITCH 10
+#define POWER_SWITCH 2
 #define NEXT 6
 #define PREV 5
 
@@ -13,12 +15,26 @@
 
 #define BRIGHTNESS  200
 #define FRAMES_PER_SECOND 60
-int j = 0;
 bool gReverseDirection = false;
 
+
+
+#define numColumns 6
+#define ledHeight 24
+
 CRGB leds[NUM_LEDS];
+CRGB& posleds(int row, int col){
+  int index =row+ledHeight*col;
+  if (index > NUM_LEDS) {index=144;}
+  if (index < 0) {index=0;};
+  return leds[index];
+}
+
 
 void setup() {
+Serial.begin(9600);//USB cable
+ Serial.println("------setup start------");
+  off = false;
   pinMode(POWER_ON, OUTPUT);
   pinMode(POWER_SWITCH, INPUT_PULLUP);
   pinMode(NEXT, INPUT_PULLUP);
@@ -27,42 +43,56 @@ void setup() {
 
   digitalWrite(POWER_ON, HIGH);
   digitalWrite(ONBOARD_LED, HIGH);
-  delay(1000);
+  
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness( BRIGHTNESS );
-  Serial.println("setup done");
+  attachInterrupt(0,offcheck,CHANGE);
+  delay(1000);
+  Serial.println("------setup done------");
 }
 
 void loop() {
-  for ( int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Blue;
-    FastLED.show(); // display this frame
-    delay(100);
-    j += 1;
-    for (int k =i; k>0 ; k--)
-    {
-      leds[k].fadeToBlackBy(abs(i-k));
+  if (!off){
+    for ( int i = 0; i < ledHeight; i++) {  
+      for (int j =0; j<numColumns; j++){
+        if (j%2>0) {
+        posleds(i,j) = CRGB::Blue;
+        }
+        else{posleds(i,j) = CRGB::Red;
+        }
+        
+      }
+      for (int k =0; k< ledHeight ; k++)
+      {
+        for (int j =0; j<numColumns; j++){
+          posleds(k,j).fadeToBlackBy(abs(i-k)*100);
+        }
+      }
+      FastLED.show(); // display this frame
+      delay(1000);
     }
-  
-    offcheck();
-  }
-
   FastLED.delay(1000 / FRAMES_PER_SECOND);
+  }
 
 }
 
 
 void offcheck()
 {
-  if (!digitalRead(10)) {
+  
+  if (!digitalRead(10) or off) {
 //    delay(5000);
     digitalWrite(ONBOARD_LED, LOW);
 //    delay(5000);
     digitalWrite(POWER_ON, LOW);
+    FastLED.clear(true);
+    Serial.println("offcheck_off");
+    off = true;
   }
   else
   {
     digitalWrite(ONBOARD_LED, HIGH);
     digitalWrite(POWER_ON, HIGH);
+    // Serial.println("offcheck_on");
   }
 }
